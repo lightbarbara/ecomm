4573a66805ff895b663544730548fc05eb5effdc
Luz Teixeira Barbara Ribeiro Teixeira <n134412@gruposantander.com>
2023-06-09 17:11:30 -0300

    feat: user stats

---
+++ mongo/scripts/estatisticasUsuario.js
@@ -0,0 +1,16 @@
+use('ecomm')
+
+const orders = db.orders.aggregate([
+    { $match: { 'account.usuario': 'barbara' } },
+    { $unwind: '$itens' },
+    {
+        $group: {
+            _id: '$_id',
+            somaQuantidades: { $sum: '$itens.quantity' },
+            montantePedidos: { $sum: { $multiply: [{ $toInt: '$itens.precoUnitario' }, '$itens.quantity'] } },
+            montanteDesconto: { $sum: { $toInt: '$itens.discount' } }
+        }
+    }
+])
+
+console.log(orders)
