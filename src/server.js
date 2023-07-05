b8f3a76d75d36a381b46e33abec0b36f1c90893b
Luz Teixeira Barbara Ribeiro Teixeira <n134412@gruposantander.com>
2023-07-05 19:26:17 -0300

    feat: swagger route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
 import app from './app.js';
 
 const port = process.env.PORT || 5000;
+
+const file = fs.readFileSync('swagger/ecomm.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Server is up and listening on port ${port}`));
